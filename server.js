e1a3add1bec4248dcf6f95a7c9a4787b5e9e76c6
IK10101 <ishitkumar3009@gmail.com>
2026-06-19 21:47:54 +0530

    Setup express server with health and ping routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/health',(req,res) =>{
+    res.json({status : 'OK',messsage : 'Fintech API is running'});
+});
+
+app.get('/ping',(req,res) =>{
+    res.json({pong  : true, timestamp : Date.now()});
+});
+
+
+
+app.listen(3000,()=>
+    console.log('Server running on port 3000')
+);
+
+
