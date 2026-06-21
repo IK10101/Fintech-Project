523fe316ef9d5ce22b4cf2a2ccf091f341afa128
IK10101 <ishitkumar3009@gmail.com>
2026-06-21 13:35:43 +0530

    Add login route with JWT token generation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const router = express.Router();
 
@@ -47,5 +48,52 @@ router.post('/register' , async (req,res)=>{
         });
     }
 })
+
+
+router.post('/login', async (req,res) => {
+    try {
+        const {email, password} = req.body;
+    
+
+    if (!email || !password){
+            return res.status(400).json({ error : 'Email and Password are required'});
+        }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(404).json({ error : 'User Not found'});
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+
+    const token = jwt.sign(
+        {userId: user.id},
+        process.env.JWT_SECRET,
+        {expiresIn: '7d'}
+    );
+
+
+    res.json({
+        message: 'Login Successful',
+        token,
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            balance: user.balance
+        }
+    });
+
+    } catch(err){
+         res.status(500).json({error: err.message});
+    }
+
+});
 
 module.exports = router;
